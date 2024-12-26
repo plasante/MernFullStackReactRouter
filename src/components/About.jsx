6ad9718e22cd72e25901fc614c969fac2da67db0
Pierre Lasante <pierre.lasante@videotron.ca>
2024-12-26 10:03:09 -0500

    Implemented Modal.css

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
+import './Modal.css';
 
 const About = () => {
   return (
-    <div
-      className={'ui raised very padded text container segment'}
-      style={{marginTop: '80px'}}
-    >
-      <h3 className={'ui header'}>About</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto consequatur cumque ducimus earum eum eveniet excepturi facere fugiat inventore, laboriosam libero maiores, modi nemo odio quam quidem recusandae soluta tempora?</p>
+    <div>
+      <div className="wrapper">
+        <div className={'modal'}>
+          <div className={'modal-content'}>
+            <h3>I am a modal</h3>
+          </div>
+        </div>
+      </div>
+      <div
+        className={'ui raised very padded text container segment'}
+        style={{marginTop: '80px'}}
+      >
+        <h3 className={'ui header'}>About</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit?</p>
+      </div>
     </div>
+
   );
 };
 

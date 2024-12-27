49ec41065da7d402a30ccbfb768b57552d419a38
Pierre Lasante <pierre.lasante@videotron.ca>
2024-12-27 07:32:18 -0500

    Connect Contact.jsx with the store

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,31 +1,37 @@
 import React from 'react';
 import {Link} from "react-router-dom";
-import Modal from "./Modal";
+// import Modal from "./Modal";
+import { connect } from "react-redux";
 
-const Contact = (props) => {
+const Contact = ({cards}) => {
   return (
     <div>
-      <Modal/>
-      <div
-        className={'ui raised very padded text container segment'}
-        style={{marginTop: '80px'}}
-      >
-        <Link to={'/pierre'} className={'ui header'}>Pierre</Link>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto consequatur cumque ducimus earum eum
-          eveniet excepturi facere fugiat inventore, laboriosam libero maiores, modi nemo odio quam quidem recusandae
-          soluta tempora?</p>
-      </div>
-      <div
-        className={'ui raised very padded text container segment'}
-        style={{marginTop: '80px'}}
-      >
-        <Link to={'/karo'} className={'ui header'}>Karo</Link>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto consequatur cumque ducimus earum eum
-          eveniet excepturi facere fugiat inventore, laboriosam libero maiores, modi nemo odio quam quidem recusandae
-          soluta tempora?</p>
-      </div>
+      {/*<Modal/>*/}
+      {
+        cards.map((card) => {
+          return (
+            <div
+              className={'ui raised very padded text container segment'}
+              style={{marginTop: '80px'}}
+              key={card.id}
+            >
+              <Link to={`/${card.title}`} className={'ui header'}>
+                {card.title}
+              </Link>
+              <p>
+                {card.body}
+              </p>
+            </div>
+          )
+        })
+      }
     </div>
   );
 };
 
-export default Contact;
+const mapStateToProps = (state) => {
+  return {
+    cards: state.cards,
+  };
+}
+export default connect(mapStateToProps)(Contact);
--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,7 +1,24 @@
 const initialState = {
-  cards: []
+  cards: [
+    {
+      id: 1,
+      title: 'Pierre',
+      body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto consequatur cumque ducimus earum eum eveniet excepturi facere fugiat inventore, laboriosam libero maiores, modi nemo odio quam quidem recusandae soluta tempora'
+    },
+    {
+      id: 2,
+      title: 'Karo',
+      body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto consequatur cumque ducimus earum eum eveniet excepturi facere fugiat inventore, laboriosam libero maiores, modi nemo odio quam quidem recusandae soluta tempora'
+    },
+    {
+      id: 3,
+      title: 'Juby',
+      body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto consequatur cumque ducimus earum eum eveniet excepturi facere fugiat inventore, laboriosam libero maiores, modi nemo odio quam quidem recusandae soluta tempora'
+    },
+  ]
+
 }
-const rootReducer = (state = {initialState}, action) => {
+const rootReducer = (state = initialState, action) => {
   return state;
 }
 

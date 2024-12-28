4a020d9cfa34a26bab0079c9a64adef4578babaa
Pierre Lasante <pierre.lasante@videotron.ca>
2024-12-28 06:32:22 -0500

    Using useSelector to get data from the store

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,17 @@
-import React, {useEffect} from 'react';
+import React from 'react';
+import { useSelector } from "react-redux";
 
 const Card = (props) => {
 
-  const [user, setUser] = React.useState('');
+  // Connecting this component to the Redux store
+  const cards = useSelector((state) => {
+    // Getting the user from the URL
+    let user = props.match.params.user
+    return state.cards.find(card => card.title === user);
+  });
 
-  useEffect(() => {
-    setUser(props.match.params.user);
-  }, []);
+  // Deconstructing the cards object
+  const { title, body } = cards;
 
   return (
     <div>
@@ -14,7 +19,8 @@ const Card = (props) => {
         className={'ui raised very padded text container segment'}
         style={{marginTop: '80px'}}
       >
-        <h3 className={'ui header'}>{user}</h3>
+        <h3 className={'ui header'}>{title}</h3>
+        <p>{body}</p>
       </div>
     </div>
   );
